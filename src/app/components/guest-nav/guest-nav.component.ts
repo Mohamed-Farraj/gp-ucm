7538fafda8002c59c54826669f4b1def13e6ed3c
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2025-02-28 18:54:15 +0200

    link navs && adding services && add font cairo in scss

--- src/app/components/guest-nav/guest-nav.component.ts
+++ src/app/components/guest-nav/guest-nav.component.ts
@@ -1,10 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-guest-nav',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass,RouterLink,RouterLinkActive],
   templateUrl: './guest-nav.component.html',
   styleUrl: './guest-nav.component.scss'
 })
---
+++ src/app/components/hero-helper/hero-helper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-helper',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-helper.component.html',
+  styleUrl: './hero-helper.component.scss'
+})
+export class HeroHelperComponent {
+
+}
