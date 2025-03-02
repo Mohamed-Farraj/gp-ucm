77372643fe59a7002e8b658930f71c529b8aa428
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2025-03-02 02:14:30 +0200

    finish hero page 2

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
 })
 export class HeroComponent {
 
+   backgroundLinks: string[] = ["./assets/heroSlider/slider-0.png","./assets/heroSlider/slider-1.jpg","./assets/heroSlider/slider-2.jpg","./assets/heroSlider/slider-3.jpg"];
 }
