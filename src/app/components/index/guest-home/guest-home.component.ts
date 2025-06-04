510966d7f009a64f47d7b0856791e42f207ad71e
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2025-06-04 14:51:23 +0300

    `feat: added async pipe and local storage service to guest home component and hero component, updated home service to inject local storage service`

--- src/app/components/index/guest-home/guest-home.component.ts
+++ src/app/components/index/guest-home/guest-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { HeroHelperComponent } from '../hero-helper/hero-helper.component';
 import { RouterOutlet } from '@angular/router';
@@ -7,6 +7,8 @@ import { GuestFooterComponent } from '../../guest-footer/guest-footer.component'
 import { ApplicationDeadlineComponent } from '../application-deadline/application-deadline.component';
 import { GuidlinesComponent } from '../guidlines/guidlines.component';
 import { AcceptanceStatusComponent } from '../acceptance-status/acceptance-status.component';
+import { LocalStorageService } from '../../../services/local-storage.service';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-guest-home',
@@ -19,11 +21,16 @@ import { AcceptanceStatusComponent } from '../acceptance-status/acceptance-statu
     GuidlinesComponent,
     AcceptanceStatusComponent,
     HeroHelperComponent,
-    HeroComponent
+    HeroComponent,
+    NgIf,
+    AsyncPipe
 ],
   templateUrl: './guest-home.component.html',
   styleUrl: './guest-home.component.scss'
 })
 export class GuestHomeComponent {
+
+     public readonly local = inject(LocalStorageService);
+  
 
 }
